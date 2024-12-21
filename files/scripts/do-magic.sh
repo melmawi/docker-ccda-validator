721e61406d4b4ea37c1a3a94a5c766952d09593c
melmawi <melmawi@aol.com>
2024-12-20 19:02:20 -0600

    Update do-magic.sh

--- files/scripts/do-magic.sh
+++ files/scripts/do-magic.sh
@@ -12,7 +12,7 @@ echo "Copying validator-api xlsx's..."
 cp /etc/submodules/code-validator-api/codevalidator-api/docs/ValueSetsHandCreatedbySITE/*.xlsx /etc/ccda/files/validator_configuration/vocabulary/valueset_repository/VSAC/
 
 echo "Downloading reference-cdda-validator war file..."
-curl -L https://github.com/siteadmin/reference-ccda-validator/releases/download/1.0.63/referenceccdaservice.war > /var/lib/tomcat/webapps/referenceccdaservice.war
+#curl -L https://github.com/siteadmin/reference-ccda-validator/releases/download/1.0.63/referenceccdaservice.war > /var/lib/tomcat/webapps/referenceccdaservice.war
 
 echo "Copying config_extras..."
 cp -f /etc/ccda/files/config_extra/web.xml /etc/tomcat/web.xml
